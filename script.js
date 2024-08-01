17b033a0e6d7424259e993ed0321b4045129d440
Russellier <russellvap13@gmail.com>
2024-08-01 19:08:03 +0800

    feat: Add user input to myLibrary

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ const dialogBox = document.querySelector('.form-container');
 const form = document.querySelector('.form');
 const addBookBtn = document.querySelector('.add-book');
 const openFormBtn = document.querySelector('.open-form');
-// const closeFormBtn = document.querySelector('.close-form');
+const closeFormBtn = document.querySelector('.close-form');
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const pages = document.querySelector('.pages');
+const read = document.querySelector('.read');
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read; //replace with boolean
+  this.read = read;
   this.info = function () {
     return `${title} by ${author}, ${pages} pages, ${read}`;
   };
@@ -28,19 +32,6 @@ function displayBooks() {
 
 if (myLibrary.length === 0) dialogBox.showModal();
 
-openFormBtn.addEventListener('click', () => {
-  dialogBox.showModal();
-});
-
-// addBookBtn.addEventLister('click', (e) => {
-//   e.preventDefault();
-//   dialogBox.close();
-// });
-
-// closeFormBtn.addEventListener('click', () => {
-//   dialogBox.close();
-// });
-
 dialogBox.addEventListener('click', (e) => {
   const dialogDimensions = dialogBox.getBoundingClientRect();
   if (
@@ -51,4 +42,26 @@ dialogBox.addEventListener('click', (e) => {
   ) {
     dialogBox.close();
   }
+});
+
+openFormBtn.addEventListener('click', () => {
+  dialogBox.showModal();
+});
+
+//fix: submitting form does not clear form for next input
+addBookBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const newBook = new Book(
+    title.value,
+    author.value,
+    pages.value,
+    read.checked
+  );
+
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+});
+
+closeFormBtn.addEventListener('click', () => {
+  dialogBox.close();
 });
