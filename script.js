e66da0f6fcbea5efdc7e7bdafd5e260cf826a9c3
Russellier <russellvap13@gmail.com>
2024-08-01 13:55:04 +0800

    feat: Create skeleton script

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  // do stuff here
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+function displayBooks() {
+  // do stuff here
+}
